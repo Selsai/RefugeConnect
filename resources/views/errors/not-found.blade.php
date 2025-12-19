b3365982016e9269ca77827a1325326f7ae563a7
Amairi <selsabilamairi@gmail.com>
2025-12-19 11:06:24 +0100

    Activité synthèse

--- resources/views/errors/not-found.blade.php
+++ resources/views/errors/not-found.blade.php
@@ -5,9 +5,6 @@
 @section('content')
     <section class="page-block">
         <h2 class="page-title">Page non trouvée.</h2>
-
-        <p>La page que vous cherchez n’existe pas.</p>
-
         <p class="back-link">
             <a href="{{ route('home') }}">Retour à l’accueil</a>
         </p>
